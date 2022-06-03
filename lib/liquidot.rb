4b85620fe200954041d47b7ccbbfaffb789c569a
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2022-06-04 00:45:35 +0200

    Add liquid rendering

--- lib/liquidot.rb
+++ lib/liquidot.rb
@@ -1,5 +1,7 @@
+require 'liquid'
 module LiquiDot
-  def self.hello_world
-    "Hello world!"
+  def self.format(raw_liquid, args=nil)
+      template = Liquid::Template.parse(raw_liquid)
+      template.render(args)
   end
 end
--- test/test_liquidot.rb
+++ test/test_liquidot.rb
@@ -2,8 +2,16 @@ require 'minitest/autorun'
 require 'liquidot'
 
 class LiquiDotTest < Minitest::Test
-  def test_hello
+  def test_format_plain_text
     assert_equal "Hello world!",
-      LiquiDot::hello_world
+      LiquiDot::format("Hello world!")
+  end
+
+  def test_format_liquid_text
+    assert_equal "Hello dave!",
+      LiquiDot::format(
+        "Hello {{ name }}!",
+        {'name' => 'dave'}
+      )
   end
 end
